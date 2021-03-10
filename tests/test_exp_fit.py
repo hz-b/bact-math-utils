Fixed derivative; offset of exponential
Added fit that also accepts and offset

tests: testing now with an offset added
init: added __all__

tests/test_exp_fit.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from bact_math_utils.exp_fit import estimate_tau_inv, fit_scaled_exp, scaled_exp
-
+from bact_math_utils.exp_fit import fit_scaled_exp_offset
 
 class Test0_ScaledExponent(unittest.TestCase):
     def test0(self):
@@ -65,8 +65,9 @@ class Test0_ScaledExponent(unittest.TestCase):
         self.assertAlmostEqual(y, np.exp(-1))
 
 
-class Test0_FitScaledExponent(unittest.TestCase):
-
+class _FitFunc:
+    '''The classical fit func
+    '''
     def evalFitFuncs(self, c, tau):
         """Calculate values and test if fit works
         """
@@ -84,6 +85,29 @@ class Test0_FitScaledExponent(unittest.TestCase):
         self.assertAlmostEqual(c_f, c)
         self.assertAlmostEqual(tau_f, tau)
 
+
+class _FitFunc:
+    '''The classical fit func
+    '''
+    def evalFitFuncs(self, c, tau):
+        """Calculate values and test if fit works
+        """
+        t = np.linspace(-10, 10)
+        y = scaled_exp(t, c, tau)
+
+        c_fe, tau_inv =  estimate_tau_inv(t, y)
+        self.assertAlmostEqual(c_fe, c)
+
+        tau_f = 1. / tau_inv
+        self.assertAlmostEqual(tau_f, tau)
+
+        pars, corr, err = fit_scaled_exp(t, y)
+        c_f, tau_f = pars
+        self.assertAlmostEqual(c_f, c)
+        self.assertAlmostEqual(tau_f, tau)
+
+
+class _TestCases_FitScaledExponent(unittest.TestCase):
     def test1(self):
         """ tau = 1, c = 1
         """
@@ -111,6 +135,52 @@ class Test0_FitScaledExponent(unittest.TestCase):
         c = 1
         tau = -1
         self.evalFitFuncs(c, tau)
+
+
+class Test0_FitScaledExponent(_TestCases_FitScaledExponent, _FitFunc):
+    '''Testing fit of exponential function
+    '''
+
+
+class _FitFuncWithOffset:
+    '''The classical fit func
+    '''
+    #: define in derived class
+    b = None
+    b_start = None
+
+    def evalFitFuncs(self, c, tau):
+        """Calculate values and test if fit works
+        """
+        t = np.linspace(-10, 10)
+        y = scaled_exp(t, c, tau)
+        y = y + self.b
+
+        b_start = self.bstart
+        pars, corr, err = fit_scaled_exp_offset(t, y, b=b_start)
+        c_f, tau_f, b = pars
+        self.assertAlmostEqual(c_f, c)
+        self.assertAlmostEqual(tau_f, tau)
+        self.assertAlmostEqual(b, self.b)
+
+class Test1_FitScaledExponentOffset(_TestCases_FitScaledExponent, _FitFuncWithOffset):
+    '''Testing fit of exponential function
+    '''
+    b = 0
+    bstart = .2
+
+class Test2_FitScaledExponentOffset(_TestCases_FitScaledExponent, _FitFuncWithOffset):
+    '''Testing fit of exponential function
+    '''
+    b = 1
+    bstart = .1
+
+
+class Test2_FitScaledExponentOffset(_TestCases_FitScaledExponent, _FitFuncWithOffset):
+    '''Testing fit of exponential function
+    '''
+    b = 10
+    bstart = 0
 
 
 if __name__ == '__main__':