13cebf67611ae46f62a807209396fa32132b3a21
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2021-11-23 15:16:02 +0100

    tune.py: added momentum calculation
    A simple one, but still one get it it wrong ...

--- bact_math_utils/tune.py
+++ bact_math_utils/tune.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Sequence
 
 _4pi = 1.0 / (4 * np.pi)
 
@@ -51,4 +52,26 @@ def tune_change(
     return dT
 
 
-__all__ = ["tune_change"]
+def tune_frequency_to_fractional_tune(
+    freq: Sequence, *, ring_frequency: float
+) -> np.ndarray:
+    freq = np.asarray(freq)
+    return freq / ring_frequency
+
+
+def to_momentum(freq, *, ref_freq, alpha):
+    """Deduces momentum from frequency
+
+    Args:
+        alpha: alpha compaction factor
+        freq:  current master clock frequency
+        ref_freq: reference frequency of the master clock
+
+    """
+    df = freq - ref_freq
+    tmp = df / (fr * alpha)
+    delta = -tmp
+    return delta
+
+
+__all__ = ["tune_change", "tune_frequency_to_fractional_tune", "to_momentum"]
